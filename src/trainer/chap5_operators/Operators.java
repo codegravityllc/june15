53b5ccc698e847a58a68393ea703d801a257bc64
Bina Shrestha <dimpybna@gmail.com>
2021-07-09 17:46:05 -0500

    test

--- src/trainer/chap5_operators/Operators.java
+++ src/trainer/chap5_operators/Operators.java
@@ -2,6 +2,8 @@ package trainer.chap5_operators;
 
 public class Operators {
 	public void test() {
-		System.out.println("hi");
+		System.out.println("hi")
+		System.out.println("This is test by Bina");;
+		
 	}
 }
